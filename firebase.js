276adfe0467c7dc0896d27f1533d1290b2ab6691
yahe97 <yajaira.huerta.97@gmail.com>
2022-04-23 22:22:23 -0500

    Add files via upload
    Detalle de firebase index

---
+++ firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyACvrHEe6QECxeFgdw28q1VxjYSUaK-wBs",
+  authDomain: "proyecto-prueba-5d824.firebaseapp.com",
+  projectId: "proyecto-prueba-5d824",
+  storageBucket: "proyecto-prueba-5d824.appspot.com",
+  messagingSenderId: "49325110242",
+  appId: "1:49325110242:web:0579704419d5b0c79affbb"
+};
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+// Exporta la funcionalidad de la base de datos
+export const firestore = firebase.firestore();
+// Exporta el paquete firebase para otros usos
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const loginConGoogle = () => auth.signInWithPopup(provider);
+export const logout = () => auth.signOut();
+export default firebase;
+//firebase.initializeApp(firebaseConfig);
+// Exporta la funcionalidad de la base de datos
+//export const firestore = firebase.firestore();
+// Exporta el paquete firebase para otros usos
+//export default firebase;
